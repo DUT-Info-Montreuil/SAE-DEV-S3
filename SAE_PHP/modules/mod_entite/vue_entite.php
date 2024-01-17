77a6d7a9be1f00f4aa60052dea1b9bed38a70d6c
hzalcman <hzalcman@gmail.com>
2024-01-17 14:05:17 +0100

    affichage liste entite

--- SAE_PHP/modules/mod_entite/vue_entite.php
+++ SAE_PHP/modules/mod_entite/vue_entite.php
@@ -14,10 +14,35 @@ class VueEntite extends VueGenerique
         <h1>Defences</h1>
         <ul><?php
             foreach ($tab_defence as $defence) {
-            ?><li><a href="index.php?module=equipes&action=details&id=<?= $equipe["id"] ?>"><?= $equipe["nom"] ?></a></li><?php
+                ?>
+                <div class="col-lg-3 col-md-4 col-sm-12">
+                    <div class="card mb-4 shadow-sm">
+                        <a href="index.php?module=entite&action=detailsEnnemi&id=<?= $defence["idDefence"]?>">
+                            <img src="img/ennemi_difficile_32x32.png" alt="Entitée 3" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $defence["nom"] ?></h5>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <li><a href="index.php?module=ennemi&action=details&id=<?= $defence["idDefence"] ?>"><?= $defence["nom"] ?></a></li><?php
                                                                                                                     }
-                                                                                                                        ?></ul>
+                                                                                                                        ?>
+        </ul>
         <h1>Ennemis</h1>
+
+        <div class="col-lg-3 col-md-4 col-sm-12">
+                        <div class="card mb-4 shadow-sm">
+                            <a href="#">
+                            <img src="img/ennemi_difficile_32x32.png" alt="Entitée 3" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">entitée 3</h5>
+                            </div>
+                            </a>
+                        </div>
+                    </div>
+                    
         <ul><?php
             foreach ($tab_ennemi as $ennemi) {
             ?>
@@ -32,7 +57,7 @@ class VueEntite extends VueGenerique
                     </div>
                 </div>
 
-                <li><a href="index.php?module=equipes&action=details&id=<?= $equipe["id"] ?>"><?= $equipe["nom"] ?></a></li><?php
+                <li><a href="index.php?module=ennemi&action=details&id=<?= $ennemi["idEnnemi"] ?>"><?= $ennemi["nom"] ?></a></li><?php
                                                                                                                     }
                                                                                                                         ?>
         </ul>
